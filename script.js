ec4e7f7bcefe8f1032c6f32aedb20fcfd5d49f21
Kadunapikin <ibakoshi27@gmail.com>
2025-05-28 11:56:37 +0100

    update scrpt.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const img = document.querySelector('img');
+const reloadBtn = document.getElementById('reload-btn');
+
+
+const loadRandomGif = () => {
+  fetch('https://api.giphy.com/v1/gifs/random?api_key=rzYxTMljevzFf9tAcXR5ZgM6kE4p1sHs&tag=&rating=g', { mode: 'cors' })
+    .then(response => response.json())
+    .then(response => {
+      img.src = response.data.images.original.url;
+    });
+}
+
+// Load a random GIF on initial page load
+loadRandomGif();
+
+// Add event listener to reload button
+reloadBtn.addEventListener('click', loadRandomGif);
